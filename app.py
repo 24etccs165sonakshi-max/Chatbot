6a26fa3185ad4fa22b168f47d206ca2606383edc
Sonakshi <24etccs165.sonakshi@technonjr.org>
2026-03-17 18:44:25 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import google.generativeai as genai
+import os
+import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+
+def my_output(query):
+    response = model.generate_content(query)
+    return response.text
+
+# UI Development using streamlit
+
+
+st.set_page_config(page_title="QUERY_BOT")
+st.header("QUERY_BOT")
+input = st.text_input("Input ", key="input")
+submit = st.button("Ask your query")
+
+if submit:
+    response = my_output(input)
+    st.subheader("The Response is=")
+    st.write(response)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from boat-ai!")
+
+
+if __name__ == "__main__":
+    main()
